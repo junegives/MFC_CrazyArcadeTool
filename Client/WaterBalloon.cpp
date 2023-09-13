08ade0dcf933c1458bc3cf84b1f63137035fe843
HanUuuu <127938317+HanUuuu@users.noreply.github.com>
2023-09-14 07:41:12 +0900

    물풍선 이미지 수정

--- Client/WaterBalloon.cpp
+++ Client/WaterBalloon.cpp
@@ -38,26 +38,36 @@ int CWaterBalloon::Update(void)
 
 	if (m_Timer > m_LimitTime)
 	{
+		CObj* pFlow1 = new CFlow;
+		pFlow1->Initialize();
+		pFlow1->Set_Pos({ m_tInfo.vPos.x, m_tInfo.vPos.y, 0.f });
+		pFlow1->Set_StateKey(L"FlowStay");
+		CObjMgr::Get_Instance()->Add_Object(CObjMgr::FLOW, pFlow1);
+
 		for (int i = 1; i <= m_iLength; i++)
 		{
 			CObj* pFlow = new CFlow;
 			pFlow->Initialize();
 			pFlow->Set_Pos({ m_tInfo.vPos.x - TILECX * i, m_tInfo.vPos.y, 0.f });
+			pFlow->Set_StateKey(L"FlowLeft");
 			CObjMgr::Get_Instance()->Add_Object(CObjMgr::FLOW, pFlow);
 
 			CObj* pFlow2 = new CFlow;
 			pFlow2->Initialize();
 			pFlow2->Set_Pos({ m_tInfo.vPos.x + TILECX * i, m_tInfo.vPos.y, 0.f });
+			pFlow2->Set_StateKey(L"FlowRight");
 			CObjMgr::Get_Instance()->Add_Object(CObjMgr::FLOW, pFlow2);
 
 			CObj* pFlow3 = new CFlow;
 			pFlow3->Initialize();
 			pFlow3->Set_Pos({ m_tInfo.vPos.x, m_tInfo.vPos.y - TILECY * i, 0.f });
+			pFlow3->Set_StateKey(L"FlowUp");
 			CObjMgr::Get_Instance()->Add_Object(CObjMgr::FLOW, pFlow3);
 
 			CObj* pFlow4 = new CFlow;
 			pFlow4->Initialize();
 			pFlow4->Set_Pos({ m_tInfo.vPos.x, m_tInfo.vPos.y + TILECY * i, 0.f });
+			pFlow4->Set_StateKey(L"FlowDown");
 			CObjMgr::Get_Instance()->Add_Object(CObjMgr::FLOW, pFlow4);
 		}
 
